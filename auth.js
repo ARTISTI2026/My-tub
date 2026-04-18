1a2feeeae68d588f53f1e9a7ea39bb423c8b642b
ARTISTI2026 <junaslimma@gmail.com>
2026-04-18 19:01:43 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,8 @@
+/* THE AUTHENTICATION SYSTEM */
+const Auth = {
+    userDNA: "NAINA_MASTER_2026",
+    isLoggedIn: true,
+    checkIdentity: function() {
+        console.log("Verifying User Identity... Access Granted.");
+    }
+};
