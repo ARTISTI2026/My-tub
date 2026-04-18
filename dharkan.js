1cc05710282a7fd91bdb5189dd49c602a17e5641
ARTISTI2026 <junaslimma@gmail.com>
2026-04-18 18:49:59 +0530

    Create dharkan.js

---
+++ dharkan.js
@@ -0,0 +1,5 @@
+/* THE HEARTBEAT (Dharkan) */
+setInterval(() => {
+    console.log("Heartbeat: System is Alive. Syncing with Naina's Server...");
+    // Har 5 second mein system ko update karna
+}, 5000);
