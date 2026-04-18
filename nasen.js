457c91288f68ae2f6fb0d32f7e9f4e44d4426f1a
ARTISTI2026 <junaslimma@gmail.com>
2026-04-18 18:54:57 +0530

    Create nasen.js

---
+++ nasen.js
@@ -0,0 +1,10 @@
+/* THE NETWORK SYSTEM (Nasen) */
+const NetworkSystem = {
+    fetchVideos: async function() {
+        console.log("Veins: Fetching live blood (videos) from server...");
+        // Ye code internet se judne ka rasta banata hai
+        return fetch('https://api.nainatube.com/videos')
+            .then(res => res.json())
+            .catch(e => console.log("Offline Mode Active"));
+    }
+};
